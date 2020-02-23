140d42b636fb23bc3c1adc399950abb0c3f5ccca
woodblock123456 <siyuanivan@gmail.com>
2020-02-23 22:33:49 +0800

    adding login function, not done

--- BackendServer/views.py
+++ BackendServer/views.py
@@ -1,7 +1,45 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
 
-BASE_DIR = 'https://127.0.0.1:8000/'
+
+# Create your views here.
+def register(request):
+    request.PATH_INFO = '/register/'
+    if request.method == 'POST':
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        if password1 == password2:
+            if User.objects.filter(username=username).exists():
+                print('username taken', username)
+                request.PATH_INFO = ''
+                return render(request, 'register.html')
+            else:
+                user = User.objects.create_user(username=username, password=password1)
+                user.save()
+                print('user created ', username)
+                return redirect('/')
+        else:
+            # throw error
+            print('password not matching')
+            request.PATH_INFO = ''
+            return render(request, 'register.html')
+
+    else:
+        return render(request, 'register.html')
+
+
+def login(request):
+    request.PATH_INFO = '/login/'
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+
+    else:
+
+        return render(request, 'login.html')
 
 def home(request):
     # return HttpResponse("Hello World")
