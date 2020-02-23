c976734930609f3092f65e9c876c8bfcf6471cdf
Weiming95 <36875218+Weiming95@users.noreply.github.com>
2020-02-23 23:08:14 +0800

    Added base.html and Question page

--- BackendServer/urls.py
+++ BackendServer/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     path('questions/', include('questions.urls')),
+    path('', include('home.urls')),
 ]
